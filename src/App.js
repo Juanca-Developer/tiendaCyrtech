f4f3f0cc77516a4a183bffc4765bad3765284064
Juan Carlos Giordano <jcgiordano@gmail.com>
2021-04-12 20:36:58 -0300

    Creacion del Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenidos a Tienda Cyrtech
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <header >
+         <NavBar/>        
       </header>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import Logo from "../img/cyrtech-logo-vector.png"
+
+
+export default function NavBar() {
+  return (
+    <div>
+       
+      <Navbar bg="light" expand="lg">
+           <Navbar.Brand href="#home">
+          <img className="App-logo" src={Logo} alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+             <Nav className="mr-auto">
+               <Nav.Link href="#empresa">Empresa</Nav.Link>
+               <Nav.Link href="#servicios">Servicios</Nav.Link>
+               <Nav.Link href="#productos">Productos</Nav.Link>
+               <Nav.Link href="#contacto">Contacto</Nav.Link>
+             </Nav>
+             
+          </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+
